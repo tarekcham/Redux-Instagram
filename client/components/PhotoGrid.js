8df12f967093f2496c76dab4ea50e41334787cce
tarekcham <tarek.alkhatib1@gmail.com>
2018-05-17 16:48:11 +0200

    routing establised

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,16 @@
+import React, { Component} from 'react';
+import { Link } from 'react-router';
+
+class PhotoGrid extends Component {
+	render() {
+		return (
+			<div className="photo-grid">
+				I'm the Photo Grid
+			</div>
+
+		);
+	}
+};
+export default PhotoGrid;
+
+
---
+++ client/components/Single.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+class Single extends Component {
+	render() {
+		return (
+			<div className="phtot-single">
+				I'm the photo Single
+			</div>
+			
+		);
+	}
+};
+export default Single;
